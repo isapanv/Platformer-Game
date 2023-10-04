2d67c3761867b316e2ad24d85b87c9441b92612b
sofyashumskikh <91876971+sofyashumskikh@users.noreply.github.com>
2023-10-04 12:33:31 +0300

    Add files via upload

--- src/main/GamePannel.java
+++ src/main/GamePannel.java
@@ -1,22 +1,35 @@
 package main;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
+
 public class GamePannel extends JPanel{
 	private MouseInputs mouseInputs;
+	private int xDelta = 100, yDelta = 100;
 	public GamePannel() {
-		mouseInputs = new MouseInputs();
-		addKeyListener(new KeyboardInputs());
+		mouseInputs = new MouseInputs(this);
+		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 	}
+	public void changeXDelta(int value) {
+		this.xDelta += value;
+		repaint();
+	}
+	public void changeYDelta(int value) {
+		this.yDelta += value;
+		repaint();
+	}
+	public void setRectPos(int x, int y) {
+		this.xDelta = x;
+		this.yDelta = y;
+		repaint();
+	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.fillRect(100, 100, 200, 50);
+		g.fillRect(xDelta, yDelta, 200, 50);
 	}
 }
